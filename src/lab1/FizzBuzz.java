dd3ca076551d5c20a1fac3c1b7978eec623aacce
JackyZhou <weige7999@gmail.com>
2026-09-14 16:02:41 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,16 +6,25 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
+        doFizzBuzz();
+    }
 
-        for (int i = 1; i <= 100; i++) {
-
+    private static void doFizzBuzz() {
+        int i = 0;
+        while(i <= 100){
+        //for (int i = 1; i <= 100; i++) {
+            //Statement 1 is executed (one time) before the execution of the code block.
+            //
+            //Statement 2 defines the condition for executing the code block.
+            //
+            //Statement 3 is executed (every time) after the code block has been executed.
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
-
+            i += 1;
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
-
+            //java's '&&' means python's 'and', java's '||' means python's 'or'
                 System.out.println("Fizz Buzz");
 
             } else if (divisibleBy3) {
